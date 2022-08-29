c94ab1b478db472dc909a49960659bb431d2768b
Stephan Heunis <s.heunis@fz-juelich.de>
2022-08-29 23:00:41 +0200

    implements datalad gooey command suite

--- datalad_gooey/gooey.py
+++ datalad_gooey/gooey.py
@@ -2,9 +2,6 @@
 
 __docformat__ = 'restructuredtext'
 
-from os.path import curdir
-from os.path import abspath
-
 from datalad.interface.base import Interface
 from datalad.interface.base import build_doc
 from datalad.support.param import Parameter
@@ -16,6 +13,11 @@ from datalad.interface.results import get_status_dict
 
 import logging
 lgr = logging.getLogger('datalad.ext.gooey.gooey')
+
+from datalad_gooey.app import GooeyApp
+from pathlib import Path
+from PySide6.QtWidgets import QApplication
+import sys
 
 
 # decoration auto-generates standard help
@@ -29,17 +31,28 @@ class Gooey(Interface):
     Long description of arbitrary volume.
     """
 
+    # usage examples
+    _examples_ = [
+        dict(
+            text=(
+                "Launch the DataLad Graphical User Interface (GUI, a.k.a Gooey) "
+                "at the specified location."
+            ),
+            code_py="gooey(path='path/to/root/explorer/directory')",
+            code_cmd="datalad gooey --path 'path/to/root/explorer/directory'",
+        ),
+    ]
+
     # parameters of the command, must be exhaustive
     _params_ = dict(
         # name of the parameter, must match argument name
-        language=Parameter(
+        path=Parameter(
             # cmdline argument definitions, incl aliases
-            args=("-l", "--language"),
+            args=("-p", "--path"),
             # documentation
-            doc="""language to say "hello" in""",
-            # type checkers, constraint definition is automatically
-            # added to the docstring
-            constraints=EnsureChoice('en', 'de')),
+            doc="""The root location from which the Gooey file explorer will be
+            launched (default is current working directory)""",
+        )
     )
 
     @staticmethod
@@ -49,26 +62,33 @@ class Gooey(Interface):
     @eval_results
     # signature must match parameter list above
     # additional generic arguments are added by decorators
-    def __call__(language='en'):
-        if language == 'en':
-            msg = 'Hello!'
-        elif language == 'de':
-            msg = 'Tachchen!'
+    def __call__(path: str = None):
+        if not path:
+            path = Path.cwd()
         else:
-            msg = ("unknown language: '%s'", language)
+            path = Path(path).resolve()
+        
+        qtapp = QApplication(sys.argv)
+        gooey = GooeyApp(path)
+        gooey.main_window.show()
+        # let a command run to have content appear in the console log
+        # uncomment for demo
+        #thread = MyThread().start()
+        sys.exit(qtapp.exec())
 
         # commands should be implemented as generators and should
         # report any results by yielding status dictionaries
+        msg = "DataLad Gooey successfully launched"
         yield get_status_dict(
             # an action label must be defined, the command name make a good
             # default
-            action='demo',
+            action='gooey',
             # most results will be about something associated with a dataset
             # (component), reported paths MUST be absolute
-            path=abspath(curdir),
+            path=path,
             # status labels are used to identify how a result will be reported
             # and can be used for filtering
-            status='ok' if language in ('en', 'de') else 'error',
+            status='ok',
             # arbitrary result message, can be a str or tuple. in the latter
             # case string expansion with arguments is delayed until the
             # message actually needs to be rendered (analog to exception
