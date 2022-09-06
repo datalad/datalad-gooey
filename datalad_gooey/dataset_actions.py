393ac35e7972d83fca777e1755aeab55cde55dc7
Michael Hanke <michael.hanke@gmail.com>
2022-09-06 10:11:02 +0200

    Group dataset method commands in the menu
    This prevents a monster menu of an endless list.
    
    For now this is only done for -core commands and their groups,
    but it could be extended in the same fashion to assign one submenu
    per extension for their commands.
    
    Closes datalad/datalad-gooey#17

--- datalad_gooey/dataset_actions.py
+++ datalad_gooey/dataset_actions.py
@@ -1,6 +1,11 @@
 from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMenu
 
-from datalad.interface.base import Interface
+from datalad.interface.base import (
+    Interface,
+    get_interface_groups,
+    load_interface,
+)
 from datalad.utils import get_wrapped_class
 
 
@@ -18,13 +23,22 @@ def add_dataset_actions_to_menu(parent, receiver, menu=None, dataset=None):
     if menu is None:
         menu = parent.sender()
 
+    group_separator = menu.addSeparator()
+
     # local import, because it is expensive, and we import from the full API
     # to ensure getting all dataset methods from any extension
     from datalad.api import Dataset
 
+    menu_lookup = _generate_submenus(menu)
+
+    # prevent reinclusion of the main menu by including it here right away
+    submenus_added = set([menu])
     # iterate over all members of the Dataset class and find the
     # methods that are command interface callables
     for mname in dir(Dataset):
+        if mname.startswith('_'):
+            # skip any private stuff
+            continue
         m = getattr(Dataset, mname)
         try:
             # if either of the following tests fails, this member is not
@@ -49,7 +63,31 @@ def add_dataset_actions_to_menu(parent, receiver, menu=None, dataset=None):
         # brings up the config UI
         action.triggered.connect(receiver)
         # add to menu
-        # TODO sort and group actions by some semantics
+        # sort and group actions by some semantics
         # e.g. all commands from one extension together
-        # to avoid a monster menu
-        menu.addAction(action)
+        # to avoid a monster menu.
+        # if the menu lookup knows a better place to put a command
+        # based on the command interface class, it will be used
+        # instead of the main menu
+        target_menu = menu_lookup.get(cls, menu)
+        target_menu.addAction(action)
+        if target_menu not in submenus_added:
+            menu.insertMenu(group_separator, target_menu)
+            submenus_added.add(target_menu)
+
+
+def _generate_submenus(menu: QMenu) -> dict:
+    """Generate a lookup of cmd -> submenu
+
+    Importantly the submenus are not added to the main menu, because it
+    is not known whether they would be actually needed.
+    """
+    lookup = dict()
+
+    for id_, title, cmds in sorted(get_interface_groups(), key=lambda x: x[0]):
+        submenu = QMenu(title, parent=menu)
+        for cmd_spec in cmds:
+            cmd_cls = load_interface(cmd_spec)
+            lookup[cmd_cls] = submenu
+
+    return lookup
