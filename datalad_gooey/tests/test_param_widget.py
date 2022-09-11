f7518e67a9a1b83bcc9fb38d70e56fc2a7df6680
Michael Hanke <michael.hanke@gmail.com>
2022-09-11 10:33:34 +0200

    Change test setup to build around load_parameter_widget()
    This is the one helper used in all places to instantiate and configure
    a parameter widget. Makes little sense to go custom in the test.

--- datalad_gooey/tests/test_param_widget.py
+++ datalad_gooey/tests/test_param_widget.py
@@ -1,3 +1,6 @@
+import functools
+from pathlib import Path
+
 from PySide6.QtWidgets import (
     QApplication,
 )
@@ -7,20 +10,40 @@ from ..param_widgets import (
     StrParamWidget,
     PosIntParamWidget,
     ChoiceParamWidget,
+    PathParamWidget,
+    load_parameter_widget,
 )
+from ..param_multival_widget import MultiValueInputWidget
 
 
 def test_GooeyParamWidgetMixin():
     # can we set and get a supported value to/from any widget
     # through the GooeyParamWidgetMixin API
 
-    # TODO is this sufficient for headless CI systems?
+    # have a headless qtapp for the CI
+    # TODO make fixture
     QApplication(['test_app', '-platform', 'offscreen'])
-    for pw, val in (
-            (BoolParamWidget(), True),
-            (PosIntParamWidget(), 4),
-            (StrParamWidget(), 'dummy'),
-            (ChoiceParamWidget(['a', 'b', 'c']), 'b'),
+    for pw_factory, val, default in (
+            (BoolParamWidget, True, False),
+            (PosIntParamWidget, 4, 1),
+            (StrParamWidget, 'dummy', 'mydefault'),
+            (functools.partial(ChoiceParamWidget, ['a', 'b', 'c']), 'b', 'c'),
+            (PathParamWidget, str(Path.cwd()), 'mypath'),
+            # cannot include MultiValueInputWidget, leads to Python segfault
+            # on garbage collection?!
+            #(functools.partial(
+            #    MultiValueInputWidget, PathParamWidget),
+            # [str(Path.cwd()), 'temp'],
+            # 'mypath'),
     ):
-        pw.set_gooey_param_spec('dummy', val)
-        assert pw.get_gooey_param_spec() == {'dummy': val}
+        # this is how all parameter widgets are instantiated
+        pw = load_parameter_widget(
+            None,
+            pw_factory,
+            name='peewee',
+            docs='EXPLAIN!',
+            value=val,
+            default=default,
+        )
+        # we get the set value back, not the default
+        assert pw.get_gooey_param_spec() == {'peewee': val}
