bfa1c3b7d8c8bc67b69a3efbac0e2c4eab40b5b8
Adina Wagner <adina.wagner@t-online.de>
2022-09-12 14:52:14 +0200

    add extension logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/datalad_logo.png'
+html_logo = '_static/datalad_gooey_logo.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
