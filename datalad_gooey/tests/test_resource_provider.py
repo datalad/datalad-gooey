2ebe6d1fcdd7f078e314473e9929ea0152c5eed1
Stephan Heunis <s.heunis@fz-juelich.de>
2022-09-19 16:32:17 +0200

    adds tests for resource_provider.py

--- datalad_gooey/tests/test_resource_provider.py
+++ datalad_gooey/tests/test_resource_provider.py
@@ -1,9 +1,24 @@
 
-from ..resource_provider import gooey_resources
+from ..resource_provider import GooeyResources
+from PySide6.QtGui import QIcon
+
+gooey_resources = GooeyResources()
+
+def test_qicon_pointer():
+    # This test should run first
+    # Test whether class dict is empty before getting icon
+    assert not gooey_resources._icons
+    # Test whether class dict has correct content after getting icon
+    gooey_resources.get_best_icon('file')
+    assert hasattr(gooey_resources, '_icons')
+    assert 'file' in gooey_resources._icons
+    assert isinstance(gooey_resources._icons['file'], QIcon) 
 
 
 def test_gooey_resources():
-    # same idea works
-    gooey_resources.get_best_icon('file')
-    # no idea works too
+    # Test all current labels
+    # None
     gooey_resources.get_best_icon(None)
+    # All other options
+    for label in gooey_resources.label_to_name.keys():
+        gooey_resources.get_best_icon(label)
