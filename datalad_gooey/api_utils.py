6c8a2f204edd775a395d8d02a21f7fee6c12a9ae
Michael Hanke <michael.hanke@gmail.com>
2022-09-19 07:49:48 +0200

    More pleasant display names for create-sibling...

--- datalad_gooey/api_utils.py
+++ datalad_gooey/api_utils.py
@@ -1,7 +1,12 @@
 
 
 def get_cmd_displayname(api, cmdname):
-    return api.get(cmdname, {}).get(
+    dname = api.get(cmdname, {}).get(
         'name',
         cmdname.replace('_', ' ').capitalize()
     )
+    dname_parts = dname.split(' ')
+    if dname_parts[:2] == ['Create', 'sibling']:
+        dname = f'Create a {" ".join(dname_parts[2:])}' \
+                f'{" " if len(dname_parts) > 2 else ""}sibling'
+    return dname
