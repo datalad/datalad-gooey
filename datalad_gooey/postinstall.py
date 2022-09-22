979521854a0ab0e82e3c52ab63dadfadd4e62cb7
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2022-09-22 19:08:05 +0200

    Add a function to install datalad-gooey icon
    The svg icon file is placed in `~/.local/share/icons`

--- datalad_gooey/postinstall.py
+++ datalad_gooey/postinstall.py
@@ -7,6 +7,7 @@ from platformdirs import user_data_path
 def perform_post_install_tasks():
     if platform == "linux":
         create_desktop_file()
+        copy_icon()
     else:
         print("Nothing to do")
 
@@ -21,3 +22,17 @@ def create_desktop_file():
 
     target_path.write_text(template_path.read_text())
     print("Created desktop file in", target_path)
+
+
+def copy_icon():
+    source_path = files(
+        "datalad_gooey.resources.icons"
+    ).joinpath('datalad_gooey_logo.svg')
+    target_path = user_data_path() / "icons" / "datalad-gooey"
+
+    if not target_path.parent.exists():
+        target_path.parent.mkdir()
+
+    # svg is text & rewriting file contents is ok, no need for shutil.copy
+    target_path.write_text(source_path.read_text())
+    print("Created icon in", target_path)
