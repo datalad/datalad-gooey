edcd3fa726765ea78304ffa22563205f69df974a
Michael Hanke <michael.hanke@gmail.com>
2022-09-27 15:29:09 +0200

    Patch `clone()` to pick up changes in root directory
    Is simply touches the clone destination root path. This enables
    the FS browser to recognize the operation outcome, and possibly change
    the type of the tree item.
    
    Closes #227

---
+++ datalad_gooey/patches/clone.py
@@ -0,0 +1,37 @@
+import logging
+# ensure the next patch-set
+import datalad_next
+
+# later
+#from datalad.core.distributed import clone as mod_clone
+# but for now
+from datalad_next.patches import clone as mod_clone
+
+lgr = logging.getLogger('datalad.core.distributed.clone')
+
+
+def _pre_final_processing_(
+    *,
+    destds,
+    cfg,
+    gitclonerec,
+    remote,
+    reckless,
+):
+    yield from orig_pre_final_processing_(
+        destds=destds,
+        cfg=cfg,
+        gitclonerec=gitclonerec,
+        remote=remote,
+        reckless=reckless,
+    )
+    # touch the root dir, such that our file watcher can pick this change up
+    destds.pathobj.touch()
+
+
+# apply patch
+lgr.debug(
+    'Apply datalad-gooey patch to clone.py:_pre_final_processing_')
+# we need to preserve the original function to be able to call it in the patch
+orig_pre_final_processing_ = mod_clone._pre_final_processing_
+mod_clone._pre_final_processing_ = _pre_final_processing_
