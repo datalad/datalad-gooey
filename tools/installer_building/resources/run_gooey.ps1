2ddd93a247b8c133f2d4ccc3ccbc4db691094822
Christian Moench <christian.moench@web.de>
2022-10-16 01:12:12 +0200

    use datalad-gooey-askpass in Windows
    This commit enables the windows version of datalad-gooey
    to interact with users graphically, using `datalad-gooey-askpass`.
    
    It sets the environment variable `$env:DISPLAY`, which seems to
    be necessary in Windows to trigger the use of the asker that is
    stored in `$env:SSH_ASKPASS`, even if `$env:SSH_ASKPASS_REQUIRE`
    is to `force`.
    
    This change is done in the starter script of datalad-gooey in order
    to not mess with $DISPLAY on X11-like systems.
    
    This fixes issue #371

--- tools/installer_building/resources/run_gooey.ps1
+++ tools/installer_building/resources/run_gooey.ps1
@@ -4,4 +4,6 @@ $ScriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $env:PATH = "$env:PATH;$ScriptPath"
 
 $env:PYSIDE_DESIGNER_PLUGINS = "."
+$env:DISPLAY = "0:0"
+
 Start-Process "$ScriptPath\..\python.exe" -ArgumentList "-m datalad_gooey" -NoNewWindow -Wait
